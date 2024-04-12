62cd77c215d41cdae6af79b0aa231b78afddca73
Ananthakrishnan <ananthan@pop-os.localdomain>
2024-04-12 12:17:40 +0530

    vite config

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base:"main-project",
     server:{
 	host: '192.168.1.6',
 	port:3000,
